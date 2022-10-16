c5acc15f6a828bc494bf7c96e75d41ed7d248edb
Shrey V Desai <84085517+ShreyVDesai@users.noreply.github.com>
2022-10-16 09:49:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+TWITTER_CONSUMER_KEY = 'mutmfxm1HBhWgPmyxDjBbBwvG'
+TWITTER_CONSUMER_SECRET = 'IPCC773NAwL8yOOHPhDhqmngzNazJwcjsXVhN2qzqUziy0VpQ5'
+TWITTER_ACCESS_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAB%2BbiAEAAAAAK5DKYl68HzjVLuNGsH7A6qvs%2B7A%3DjhuCevOOWIGogl2D1XXx4fqyBnUvYb8vGZ94tvgGQ44zDQELo5'
+TWITTER_ACCESS_TOKEN_SECRET = ''
+
+TWITTER_USERNAMES = [
+    'traderstewie',
+    'the_chart_life',
+    'canuck2usa',
+    'sunrisetrader',
+    'tmltrader'
+]
+
+DB_HOST = '127.0.0.1'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
---
+++ dashboard.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import config 
+import psycopg2, psycopg2.extras
+import plotly.graph_objects as go
+import yfinance as yf
+from datetime import date
+import main
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+
+option = st.sidebar.selectbox("Which Dashboard?", ('Chart', 'Stocktwits','Predictions'))
+
+st.header(option)
+
+
+
+if option == 'Chart':
+    symbol = st.sidebar.text_input("Symbol", value='MSFT', max_chars=None, key=None, type='default')
+
+    ticker1 = yf.Ticker(symbol)
+    data = ticker1.history(symbol,start = START, end = TODAY,interval = '1d')
+    st.subheader(symbol.upper())
+
+    fig = go.Figure(data=[go.Candlestick(x=data.index,
+                    open=data['Open'],
+                    high=data['High'],
+                    low=data['Low'],
+                    close=data['Close'],
+                    name=symbol)])
+
+    fig.update_xaxes(type='category')
+    fig.update_layout(height=700)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.write(data)
+
+
+
+
+if option == 'Stocktwits':
+    symbol = st.sidebar.text_input("Symbol", value='AAPL', max_chars=5)
+
+    r = requests.get(f"https://api.stocktwits.com/api/2/streams/symbol/{symbol}.json")
+
+    data = r.json()
+
+    for message in data['messages']:
+        st.image(message['user']['avatar_url'])
+        st.write(message['user']['username'])
+        st.write(message['created_at'])
+        st.write(message['body'])
+
+
+if option == 'Predictions':
+    symbol = st.sidebar.text_input("Symbol", value='AAPL', max_chars=5)
+    st.title('Stock Forecast App')
+    n_years = st.slider('Years of data to consider for prediction:', 1, 4)
+    
+
+    pred = main.make_stock_prediction(symbol, n_years)
+    time = st.number_input('Number of days for prediction:',min_value=1,max_value=10,value=5,step=1)
+    st.write(f"The predicted closing values for next {time} days are:")
+    st.write(pred.head(time))
